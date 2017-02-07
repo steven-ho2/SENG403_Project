a0b035c53d426391e5cea2f976f747b2cca25c22
brianisbae <asianese.brian@hotmail.com>
2017-02-07 16:09:03 -0700

    added snooze button to mainwindow

---
+++ Team4Clock/Snooze.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Team4Clock;
+
+namespace Team4Clock
+{
+    public partial class Snooze
+    {
+
+        private bool test;
+        public Snooze()
+        {
+            test = true;
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
