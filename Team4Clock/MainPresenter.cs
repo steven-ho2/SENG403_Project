9f3c0b838015c66281fa9c1dd70166df19233e73
Michael Mooney <mmooney@synopsys.com>
2017-03-22 15:26:39 -0600

    Added basic classes for MVVM adaptation.

---
+++ Team4Clock/MainPresenter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team4Clock
+{
+    class MainPresenter : ObservableObject
+    {
+    }
+}
---
+++ Team4Clock/ObservableObject.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace Team4Clock
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChangedEvent(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
